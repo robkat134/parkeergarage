11e4e9500fb9091bac68051ed64157a3da2de88b
Rob <Rob@145.37.128.101>
2017-01-16 11:06:41 +0100

    Werkende versie van Blackboard

---
+++ parkeergarage/src/parkeergarage/Main.java
@@ -0,0 +1,11 @@
+package parkeergarage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Simulator test = new Simulator();
+		test.run();
+	}
+
+}
