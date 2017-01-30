15b390577d3bd1347be5a9b79fa2646321069feb
Rutger Scholma <rutger-100@hotmail.com>
2017-01-30 11:32:03 +0100

    Lineview geupdate door Winter

--- parkeergarage/src/parkeergarage/LineView.java
+++ parkeergarage/src/parkeergarage/LineView.java
@@ -3,10 +3,13 @@ package parkeergarage;
 import java.awt.*;
 import java.util.ArrayList;
 
+import javax.sound.sampled.Line;
+
 public class LineView extends View {
 	
 	Simulator simulator;
 	ArrayList<Rect> line = new ArrayList<Rect>();
+	ArrayList<Rect> line2 = new ArrayList<Rect>();
 	public LineView(Model model, Simulator Tempsimulator) {
 		super(model);
 		simulator = Tempsimulator;
@@ -16,7 +19,8 @@ public class LineView extends View {
 		System.out.println("painting");
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, 1000, 100);
-		g.setColor(Color.BLUE);
+		
+		g.setColor(Color.RED);
 		System.out.println("paint component word aangeroepen");
 		addRectangle();
 		System.out.println(line.size());
@@ -24,10 +28,16 @@ public class LineView extends View {
 		{
 			g.fillRect(line.get(i).x1,line.get(i).y1,line.get(i).x2,line.get(i).y2); 
 		}
+
+		g.setColor(Color.BLUE);
+		for(int i=0;i<line2.size();i++)
+		{
+			g.fillRect(line2.get(i).x1,line2.get(i).y1,line2.get(i).x2,line2.get(i).y2); 
+		}
 	}
 	public void addRectangle() 
 	{
-		line.add(0,new Rect(startX,(int)(simulator.AantalVrijePlekken/5.4), 1, (int)(simulator.TotaalAantalPlekken-simulator.AantalVrijePlekken/5.4)));
+		line.add(0,new Rect(startX,(int)(simulator.geparkeerdeZonderAbonnement/5.4), 2, (int)((simulator.TotaalAantalPlekken-simulator.geparkeerdeZonderAbonnement)/5.4)));
 		System.out.println(line.size());
 		if (line.size()>200)
 		{
@@ -38,5 +48,16 @@ public class LineView extends View {
 			}
 			line.remove(line.size()-1);
 		}
+		line2.add(0,new Rect(startX,(int)(simulator.geparkeerdeAbonnementhouders/5.4), 2, (int)((simulator.TotaalAantalPlekken-simulator.geparkeerdeAbonnementhouders)/5.4)));
+		if (line2.size()>200)
+		{
+			for(int i=0;i<line2.size();i++)
+			{
+				line2.get(i).x1--;
+				startX--;
+			}
+			line2.remove(line2.size()-1);
+		}
 	}	
 }
+
