8a0c05c12ecbd81b3aed053c74c856069089fd08
Rutger Scholma <rutger-100@hotmail.com>
2017-01-30 11:28:49 +0100

    Mijn zelf gemaakte pieview

---
+++ parkeergarage/src/parkeergarage/PieView.java
@@ -0,0 +1,34 @@
+package parkeergarage;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+@SuppressWarnings("serial")
+public class PieView extends View {
+	Simulator simulator;
+	public PieView(Model model, Simulator tempSimulator) {
+		super(model);
+		simulator = tempSimulator;
+	}
+
+	public void paintComponent(Graphics g) {
+
+
+		g.setColor(Color.WHITE);
+		g.fillRect(890, 0, 100, 100);
+		
+		
+		int startAngle = 0;
+		double aantal=(simulator.geparkeerdeAbonnementhouders)/1.5;
+		int intAantal = (int)aantal;
+		g.setColor(Color.BLUE);
+		g.fillArc(900, 10, 80, 80, startAngle, intAantal);
+		
+		startAngle += intAantal;
+		double secondAantal = (simulator.geparkeerdeZonderAbonnement)/1.5;
+		int intSecondAantal = (int)secondAantal;
+		g.setColor(Color.RED);
+		g.fillArc(900, 10, 80, 80, startAngle, intSecondAantal);	
+	}	
+}
+
