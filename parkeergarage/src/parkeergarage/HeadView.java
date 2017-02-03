85b1f9034a2d48773d56b45b10341cc66cf85927
Winter Halbesma <whalbesma@gmail.com>
2017-02-03 17:36:40 +0100

    dagen toegevoegd onder barview

---
+++ parkeergarage/src/parkeergarage/HeadView.java
@@ -0,0 +1,33 @@
+package parkeergarage;
+
+import java.awt.Graphics;
+
+import javax.swing.*;
+
+public class HeadView extends View {
+	public JLabel parkedCars = new JLabel("Parked Cars: ");
+    public JLabel time = new JLabel("Time: ");
+    private Simulator simulator;
+    
+    public HeadView(Model model, Simulator tempSimulator) {
+    	super(model);
+    	simulator = tempSimulator;
+    	
+    	setLayout(null);
+    	setSize(838, 50);
+    	
+    	add(parkedCars);
+    	add(time);
+    	parkedCars.setBounds(300, 0, 200, 40);
+    	time.setBounds(500, 0, 200, 40);
+    }
+    
+    public void updateHeadView()
+    {
+    	parkedCars.setText("Parked cars: "+simulator.totalCarsToday);
+    }
+    
+    public void paintComponent(Graphics g) {
+    	updateHeadView();
+    }
+}
